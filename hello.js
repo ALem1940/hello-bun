474b9e93a3edd55f9e0a9bc133b5ecde05296e32
Ashanti <shamarkey03@yahoo.com>
2026-07-31 17:33:31 -0500

    start hello-bun with hello script and setup notes

---
+++ hello.js
@@ -0,0 +1,6 @@
+let course = "Level 2";
+let message = "Push through " + course + "there's gold at the end of the rainbow!"
+let myName = "Ashanti";
+console.log(message);
+console.log("This JavaScript runs on MY machine.");
+console.log("No more stopping " + myName + "!");
